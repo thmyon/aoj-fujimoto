f93663c64879b08ef97c39cdfa810a8871bbbe9f
thmyon <fujimoto@pl.info.kochi-tech.ac.jp>
2017-06-15 22:35:16 +0900

    ITP1_5Dの改良

--- ITP/ITP1_5D/src/Main.java
+++ ITP/ITP1_5D/src/Main.java
@@ -5,22 +5,21 @@ public class Main {
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
 		int x;
-		String str = "";
 		for (int i = 1; i <= n; i++) {
 			x = i;
 			if (x % 3 == 0) {
-				str += " " + x;
+				System.out.print(" " + i);
 			} else {
 				while (x != 0) {
 					if (x % 10 == 3) {
-						str += " " + i;
+						System.out.print(" " + i);
 						break;
 					}
 					x /= 10;
 				}
 			}
 		}
-		System.out.println(str);
+		System.out.println();
 	}
 
 }
